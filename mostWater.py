cc437fb159397c034e63a7f57199e8dd5441bc78
HsuJohnny <b02901113@ntu.edu.tw>
2016-11-24 11:36:29 +0900

    Add files via upload

--- mostWater.py
+++ mostWater.py
@@ -1,11 +1,20 @@
 def maxArea(height):
 	
+	maxarea=0
 	length=len(height)
-	originalH=height
-	height.sort()
-	maxLevel=height[length-1]
-	return 	
+	right=0
+	left=length-1
+	while (right != left):
+		maxarea=max(maxarea, min(height[right], height[left])*(left-right))
+		if (height[right] > height[left]):
+			left=left-1
+		else:
+			right=right+1
+	return maxarea
 
 
-print maxArea([1,2,4,3])
+print maxArea([1,2,3,4,5])	
+
+
+
 			
